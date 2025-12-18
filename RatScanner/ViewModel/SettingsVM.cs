9c587c1cd6cc54cfa7ec92243616eac11dc69786
Niv <eliotley06@gmail.com>
2025-12-18 18:38:47 +0100

    Load and apply UI language setting

--- RatScanner/ViewModel/SettingsVM.cs
+++ RatScanner/ViewModel/SettingsVM.cs
@@ -74,6 +74,7 @@ internal class SettingsVM : INotifyPropertyChanged {
 
 		ToolTipDuration = RatConfig.ToolTip.Duration.ToString();
 		ToolTipMilli = RatConfig.ToolTip.Duration;
+		UiLanguage = RatConfig.UserInterface.Language;
 
 		ShowName = RatConfig.MinimalUi.ShowName;
 		ShowAvgDayPrice = RatConfig.MinimalUi.ShowAvgDayPrice;
@@ -113,6 +114,7 @@ internal class SettingsVM : INotifyPropertyChanged {
 		bool updateTarkovTrackerBackend = TarkovTrackerBackend != RatConfig.Tracking.TarkovTracker.Backend;
 		bool updateResolution = ScreenWidth != RatConfig.ScreenWidth || ScreenHeight != RatConfig.ScreenHeight;
 		bool updateLanguage = RatConfig.NameScan.Language != (Language)NameScanLanguage;
+		bool updateUiLanguage = RatConfig.UserInterface.Language != UiLanguage;
 
 		// Save config
 		RatConfig.NameScan.Enable = EnableNameScan;
@@ -126,6 +128,7 @@ internal class SettingsVM : INotifyPropertyChanged {
 
 		RatConfig.ToolTip.Duration = int.TryParse(ToolTipDuration, out int i) ? i : 0;
 		RatConfig.ToolTip.Duration = ToolTipMilli;
+		RatConfig.UserInterface.Language = UiLanguage;
 
 		RatConfig.MinimalUi.ShowName = ShowName;
 		RatConfig.MinimalUi.ShowAvgDayPrice = ShowAvgDayPrice;
@@ -161,6 +164,7 @@ internal class SettingsVM : INotifyPropertyChanged {
 		PageSwitcher.Instance.ResetWindowSize();
 		await TarkovDevAPI.InitializeCache();
 		if (updateTarkovTrackerToken || updateTarkovTrackerBackend) UpdateTarkovTrackerToken();
+		if (updateUiLanguage) _localizationService.SetLanguage(UiLanguage);
 		if (updateResolution || updateLanguage) RatScannerMain.Instance.SetupRatEye();
 
 		RatEye.Config.LogDebug = RatConfig.LogDebug;
